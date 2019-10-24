5c2ce65ca497454bb84c4a33689f066e25ac092f
Mateusz Armatys <mateusz.armatys@gmail.com>
2019-10-24 14:39:37 +0200

    Update libraries

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -43,9 +43,10 @@ dependencies {
 
     implementation(project(":empress_android"))
     implementation(project(":empress_builder"))
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.2.0-beta01")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.2.0-rc01")
     implementation(Deps.appCompat)
     implementation(Deps.constraintLayout)
+    implementation(Deps.fragment)
     implementation(Deps.kotlinStdLib)
 
     testImplementation(Deps.coroutinesTest)
