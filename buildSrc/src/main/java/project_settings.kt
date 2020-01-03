2c6344c0fcf0df9caa3f45973d07219d14b8641b
Mateusz Armatys <mateusz@nofrills.io>
2020-01-03 16:18:59 +0100

    Version 0.5.0 (5)

--- buildSrc/src/main/java/project_settings.kt
+++ buildSrc/src/main/java/project_settings.kt
@@ -24,8 +24,8 @@ object EmpressLib {
     const val mavPluginVersion = "1.0.4"
     const val minSdkVersion = 21
     const val targetSdkVersion = 28
-    const val versionCode = 4
-    const val versionName = "0.4.0"
+    const val versionCode = 5
+    const val versionName = "0.5.0"
 }
 
 object Ver {
