3d0c8e7f48071c4526c4ea95906691f67c650ead
Mateusz Armatys <mateusz@nofrills.io>
2020-04-10 12:48:17 +0200

    Fix tests

--- test_support/src/main/kotlin/io/nofrills/empress/test_support/SampleFragment.kt
+++ test_support/src/main/kotlin/io/nofrills/empress/test_support/SampleFragment.kt
@@ -2,20 +2,20 @@ package io.nofrills.empress.test_support
 
 import androidx.fragment.app.Fragment
 import io.nofrills.empress.android.enthrone
-import io.nofrills.empress.base.EmpressApi
+import io.nofrills.empress.base.TestEmpressApi
 import kotlinx.coroutines.CoroutineDispatcher
 
 class SampleFragment : Fragment(), WithEmpress {
     override fun enthroneEmpress(
         dispatcher: CoroutineDispatcher,
         retainInstance: Boolean
-    ): EmpressApi<SampleEmpress, Model, Signal> {
+    ): TestEmpressApi<SampleEmpress, Model, Signal> {
         return enthrone(
             "sample_empress",
             SampleEmpress(),
             eventDispatcher = dispatcher,
             requestDispatcher = dispatcher,
             retainInstance = retainInstance
-        )
+        ) as TestEmpressApi<SampleEmpress, Model, Signal>
     }
 }
--- test_support/src/main/kotlin/io/nofrills/empress/test_support/WithEmpress.kt
+++ test_support/src/main/kotlin/io/nofrills/empress/test_support/WithEmpress.kt
@@ -1,11 +1,11 @@
 package io.nofrills.empress.test_support
 
-import io.nofrills.empress.base.EmpressApi
+import io.nofrills.empress.base.TestEmpressApi
 import kotlinx.coroutines.CoroutineDispatcher
 
 interface WithEmpress {
     fun enthroneEmpress(
         dispatcher: CoroutineDispatcher,
         retainInstance: Boolean
-    ): EmpressApi<SampleEmpress, Model, Signal>
+    ): TestEmpressApi<SampleEmpress, Model, Signal>
 }
